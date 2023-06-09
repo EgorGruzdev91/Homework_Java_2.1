87f48c80404d34e6096d629770c270df7d5b20ff
Egor Gruzdev <gruzdev_e.i@mail.ru>
2023-06-09 08:57:22 +0300

    Correction 1 09/06/23

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 13676;
-        int bonus_point = 20;
+        int bonusPoint = 20;
 
-        int Bonus = (amount / bonus_point);
+        int Bonus = (amount / bonusPoint);
         System.out.println("Вам начислено " + Bonus + " бонуса(ов)");
 
     }
